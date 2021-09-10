e2098e035c39cafdb7377a5250b9973cbb1e30bc
Idan Zohar <darkyyy@gmail.com>
2021-09-10 18:45:43 +0300

    Added missing information handling

--- javascript.js
+++ javascript.js
@@ -1,21 +1,26 @@
-console.log('loaded');
+//Constants
 const searchButton = document.getElementById("btnSearch");
 const searchBox = document.getElementById("txtSearch");
 const resultsArea = document.getElementById("spnResults");
 
 
 let renderResults =(data) => {
-    for(var i=0; i<data.items.length; i++) {
+    let bookImage,bookDesc;
+    resultsArea.innerHTML = "";
+    for(let i=0; i<data.items.length; i++) {
         console.log(data.items);
-        resultsArea.innerHTML += `<h2><img src="${data.items[i].volumeInfo.imageLinks.thumbnail}"></h2>`;
-        resultsArea.innerHTML += `<h2>${data.items[i].volumeInfo.title}</h2>`;
-        resultsArea.innerHTML += `<h2>${data.items[i].volumeInfo.description}</h2>`;
+        // Check if there's a book image
+        if (data.items[i].volumeInfo.imageLinks) {bookImage=`<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}">`} else {bookImage = "<p>No Image</p>"}
+        // Check if there's a book description
+        if (data.items[i].volumeInfo.description) {bookDesc=data.items[i].volumeInfo.description} else {bookDesc = "No Description"}
+        // Render Book
+        resultsArea.innerHTML += `<h2 class="card"> ${bookImage} <p>${data.items[i].volumeInfo.title}</p> <p>${bookDesc}</p></h2>`;
     }
 }
 
 let searchFunction = (evt)=> {
     if (searchBox.value) {
-    alert("Searching");
+        searchButton.value = "Searching"
     let input = searchBox.value;
 
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}`)
