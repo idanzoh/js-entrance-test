4948b562fc154c052a2d528fb985322ddfda7356
Idan Zohar <darkyyy@gmail.com>
2021-09-11 15:39:22 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,40 +1,52 @@
-//Constants
-const searchButton = document.getElementById("btnSearch");
-const searchBox = document.getElementById("txtSearch");
-const resultsArea = document.getElementById("spnResults");
+    //Constants
+    const searchButton = document.getElementById("btnSearch");
+    const searchBox = document.getElementById("txtSearch");
+    const resultsArea = document.getElementById("spnResults");
 
 
-let renderResults =(data) => {
-    let bookImage,bookDesc;
-    resultsArea.innerHTML = "";
-    for(let i=0; i<data.items.length; i++) {
-        console.log(data.items);
-        // Check if there's a book image
-        if (data.items[i].volumeInfo.imageLinks) {bookImage=`<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}">`} else {bookImage = "<p>No Image</p>"}
-        // Check if there's a book description
-        if (data.items[i].volumeInfo.description) {bookDesc=data.items[i].volumeInfo.description} else {bookDesc = "No Description"}
-        // Render Book
-        resultsArea.innerHTML += `<h2 class="card"> ${bookImage} <p>${data.items[i].volumeInfo.title}</p> <p>${bookDesc}</p></h2>`;
+    let renderResults =(data) => {
+        let bookImage,bookDesc;
+        searchButton.innerText = "Search"
+        searchBox.value = ""
+        resultsArea.innerHTML = "";
+        for(let i=0; i<data.items.length; i++) {
+            // Check if there's a book image
+            if (data.items[i].volumeInfo.imageLinks) {bookImage=`<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}">`} else {bookImage = "<p>No Image</p>"}
+            // Check if there's a book description
+            if (data.items[i].volumeInfo.description) {bookDesc=data.items[i].volumeInfo.description} else {bookDesc = "No Description"}
+            // Render Book
+            resultsArea.innerHTML += `<h2 class="card"> <div>${bookImage}</div> <p>${data.items[i].volumeInfo.title}</p> <p class="bookDesc">${bookDesc}</p></h2>`;
+        }
     }
-}
 
-let searchFunction = (evt)=> {
-    if (searchBox.value) {
-        searchButton.value = "Searching"
-    let input = searchBox.value;
+    let searchFunction = (evt)=> {
+        if (searchBox.value) {
+            searchButton.innerText = "Searching"
+        let input = searchBox.value;
 
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}`)
-    .then(response => response.json())
-    .then(data => renderResults(data));
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}`).then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error('Something went wrong');
+            }
+          })
+          .then((responseJson) => {
+            // Do something with the response
+            renderResults(responseJson)
+          })
+          .catch((error) => {
+            console.log(error)
+          });
+        
+        evt.preventDefault();
+    }
 
-    evt.preventDefault();
-}
+    else {
+        evt.preventDefault();
+        searchBox.classList.add("warning");
+    }
+    }
 
-else {
-    alert("Search box is empty!");
-}
-}
-
-
-
-searchButton.addEventListener("click", searchFunction);
+    searchButton.addEventListener("click", searchFunction);
+    searchBox.addEventListener("click", ()=> {searchBox.classList.remove("warning");})
